f91491a3ff5692f180eda4b054cf1a0932003022
Dev1600 <devanshmehra16@gmail.com>
2022-02-14 23:40:21 +0530

    Basic R function added

--- helloR.R
+++ helloR.R
@@ -1,2 +1,17 @@
-print("This file was created within RStudio")
-print("And now it lives on GitHub")
+## This is a comment
+x<-20
+msg<-"Hello World"
+## below works similar to python
+y<-1:20
+print(x)
+print(msg)
+print(y)
+## By default no. are of double precision by suffix L they become long type
+n<-NaN
+b<-20
+a<-20L
+print(n)
+print(b)
+print(a)
+print(attributes(a))
+print(attributes(b))
